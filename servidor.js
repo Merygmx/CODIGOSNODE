d5a28d781985f46b426aa7745be337837b426c16
Merygmx <merygmx@gmail.com>
2024-12-09 12:51:51 -0600

    Create servidor.js

---
+++ servidor.js
@@ -0,0 +1,11 @@
+//crear servidor HTTP sencillo
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('hola, Node.js!\n');});
+
+    server.listen(3000,'127.0.0.1',()=>{
+        console.log('Servidor en ejecución en http://127.0.0.1:3000/');
+    });
